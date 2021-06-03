d6cb489fe62c1ff5e6a799badbffcd4cba829c95
ajengri <ajengri04@gmail.com>
2021-06-03 18:40:58 +0700

    Projek Arsip Alumni Dengan Database

---
+++ database/migrations/2021_06_03_080722_create_article_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul', 255);
+            $table->string('image')->unique();
+            $table->string('deskripsi', 455);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
---
+++ database/migrations/2021_06_03_081240_create_kontak_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKontakTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kontak', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('whatsapp',20)->unique();
+            $table->string('pesan',455);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kontak');
+    }
+}
