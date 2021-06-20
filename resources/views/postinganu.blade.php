c8e6e046bc9f1359be13b2a35ab85386bc16e6e0
rizqims <rizqims100@gmail.com>
2021-06-20 16:44:34 +0700

    update postingan

--- resources/views/postinganu.blade.php
+++ resources/views/postinganu.blade.php
@@ -13,14 +13,13 @@
 
     /* new */
     .card {
-        margin-right: 10px;
         margin-top: 10px;
-        float: left;
-        /* display: flex; */
     }
-    .wrapper{
+
+    .wrapper {
         padding: 10px 10px;
     }
+
     #profil-pic {
         width: 50px;
         height: 50px;
@@ -28,72 +27,72 @@
         float: left;
         position: relative;
     }
-    .dropdown-item{
+
+    .dropdown-item {
         height: 0.7cm;
     }
 
+    .yang-lalu {
+        font-size: 9pt;
+        color: rgb(65, 65, 65);
+    }
+
+
 </style>
 @section('container')
-    <div style="margin: 0 35px;" class="">
+    <div class="wrapper">
+        <div class="sidebar">
 
-        <br>
-        <br>
-        <h3>Postingan</h3>
-        <br>
-        <div class="card" style="width: 30rem;">
-            <div class="card-header">
-                <img src="{{ asset('/img/team/unknown.png') }}" alt="" id="profil-pic">
+        </div>
+        <div style="margin: 0 40px;" class="">
+            <br>
+            <br>
+            <h3>Postingan</h3>
+            <br>
+            <div class="card" style="width: 30rem;">
+                <div class="card-header">
+                    <img src="{{ asset('/img/team/unknown.png') }}" alt="" id="profil-pic">
 
-                {{-- <button class="btn" >
-                    <i style="float: left" class="fas fa-flag"></i><p>&nbsp&nbsp&nbspreport</p>
-                </button> --}}
-                <button style="float: right; border-style: none; background: none;"><i class="fas fa-flag"></i></button>
+                    {{-- <button class="btn" >
+                        <i style="float: left" class="fas fa-flag"></i><p>&nbsp&nbsp&nbspreport</p>
+                    </button> --}}
+                    <button style="float: right; border-style: none; background: none;"><i class="fas fa-flag"></i></button>
 
 
 
-                <div class="dropdown">
-                    <button class="btn btn-sm float-end" id="droppy" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-share"></i></button>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="droppy">
-                        <li><a href="#" class="dropdown-item"><i style="float: left" class="fas fa-share"></i><p>&nbsp&nbsp&nbspshare</p></a></li>
-                    </ul>
+                    <div class="dropdown">
+                        <button class="btn btn-sm float-end" id="droppy" data-bs-toggle="dropdown" aria-expanded="false"><i
+                                class="fa fa-share"></i></button>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="droppy">
+                            <li><a href="#" class="dropdown-item"><i style="float: left" class="fas fa-share"></i>
+                                    <p>&nbsp&nbsp&nbspshare</p>
+                                </a></li>
+                        </ul>
+                    </div>
+                    <h5 class="card-title">Asep Kurnia</h5>
+                    <p class="card-subtitle yang-lalu">19 menit yang lalu</p>
                 </div>
-                {{-- <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown button
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </div> --}}
-
-
-
-
-
-                <h5 class="card-title">Asep Kurnia</h5>
-                <h6 class="card-subtitle">Angkatan 19</h6>
+                <div class="wrapper">
+                    <img src="{{ asset('/img/team/team-1.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-bold">perayaan idul adha</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's
+                            content.</p>
+                    </div>
+                </div>
             </div>
-            <div class="wrapper">
-                <img src="{{ asset('/img/team/team-1.jpg') }}" class="card-img-top" alt="...">
+
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title text-bold">perayaan idul adha</h5>
+                    <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                         card's
                         content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-            </div>
-        </div>
-
-
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     </div>
