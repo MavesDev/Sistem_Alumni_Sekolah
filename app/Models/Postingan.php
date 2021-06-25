11a4533175473d0c2a4a94a96b9c70f8572ff8f6
ajengri <ajengri04@gmail.com>
2021-06-25 09:00:47 +0700

    Menambahkan fitur create dan delet postingan

---
+++ app/Models/Postingan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Postingan extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'post_name',
+        'post_course',
+        'post_desc',
+        'post_image'
+    ];
+}
